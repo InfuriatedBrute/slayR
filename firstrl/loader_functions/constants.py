1995b20f2e7aee24537a08772adf51702cb0823a
DustinBrugler <brugler@otterbein.edu>
2019-04-09 09:38:37 -0400

    config -> edit, moved run.bat's error log

--- firstrl/loader_functions/constants.py
+++ firstrl/loader_functions/constants.py
@@ -2,9 +2,9 @@ from os.path import curdir
 
 from loader_functions.json_loaders import load_json
 
-constants_dir = curdir + "\\constants\\"
-config = load_json(constants_dir + "config.json")
-colors = load_json(constants_dir + "colors.json")
-items = load_json(constants_dir + "items.json")
-enchantments = load_json(constants_dir + "enchantments.json")
-item_prefixes = load_json(constants_dir + "item_prefixes.json")
+edit_dir = curdir + "\\edit\\"
+config = load_json(edit_dir + "config.json")
+colors = load_json(edit_dir + "colors.json")
+items = load_json(edit_dir + "items.json")
+enchantments = load_json(edit_dir + "enchantments.json")
+item_prefixes = load_json(edit_dir + "item_prefixes.json")
